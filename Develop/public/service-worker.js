487b716e9780787bbb1a1042f9b60e3b20553310
Jonah Milner <jonahmilner86@gmail.com>
2022-06-03 12:21:23 -0600

    fixed an issue with the service-worker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -38,17 +38,6 @@ self.addEventListener('install', function (e) {
     );
   });
 
-  caches.match(e.request).then(function (request) {
-    if (request) {
-      console.log('responding with cache : ' + e.request.url)
-      return request
-    } else {
-      console.log('file is not cached, fetching : ' + e.request.url)
-      return fetch(e.request)
-  }
-
-  })
-
 
   self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
